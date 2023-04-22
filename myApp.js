42035744c20176192737ec879ceedbfd4bb11be1
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-04-22 18:34:28 -0400

    using express.static method

--- myApp.js
+++ myApp.js
@@ -12,3 +12,9 @@ module.exports = app;
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
